aecfef6b0b1fae179cfc7d932add7b9315b1068e
boredland <boredland@users.noreply.github.com>
2015-02-27 14:40:58 +0100

    Update aio_install.sh

--- aio_install.sh
+++ aio_install.sh
@@ -13,13 +13,13 @@ sudo apt-get install python2.7 python2.7-dev python-virtualenv \
 python-tk idle python-pmw python-imaging \
 python-pip libpython2.7-dev libusb-dev \
 libjpeg-dev libtiff5-dev libqt4-core ruby-rmagick \
-libmagickwand-dev \
-ruby-hpricot lua5.2 git imagemagick build-essential \
+libmagickwand-dev lua5.2 liblua5.2-dev \
+ruby-hpricot git imagemagick build-essential \
 libqt4-dev libjpeg8-dev libjpeg-turbo8-dev libjpeg-dev git \
 bash-completion nfs-common ghostscript ruby1.9.1 ruby1.9.1-dev \
 rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 libopenssl-ruby1.9.1 libssl-dev \
-zlib1g-dev subversion cmake zlib1g-dev libpng12-dev libtiff5-dev \
-libboost1.55-all-dev libxrender-dev liblua5.2-dev \
+zlib1g-dev cmake zlib1g-dev libpng12-dev libtiff5-dev \
+libboost1.55-all-dev libxrender-dev \
 automake libtool libpango1.0-dev -y
 
 ##install latest leptonica
