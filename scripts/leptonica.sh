c7da1ddf5bb7ef47467e8e5eb13d63685c43047b
boredland <boredland@users.noreply.github.com>
2015-03-03 21:03:56 +0100

    Update leptonica.sh

--- scripts/leptonica.sh
+++ scripts/leptonica.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 set -x
+if [[ $1 == 1 ]]; then exit 0;fi
 command -v convertfilestopdf >/dev/null 2>&1 || {
 wget http://www.leptonica.com/source/leptonica-1.71.tar.gz
 tar xvf leptonica-1.71.tar.gz
