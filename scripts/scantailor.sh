4e2dafd06eafc902e6271b5368a302e912c5620c
boredland <boredland@users.noreply.github.com>
2015-03-03 19:49:26 +0100

    Update scantailor.sh

--- scripts/scantailor.sh
+++ scripts/scantailor.sh
@@ -6,6 +6,6 @@ wget -O scantailor-enhanced-20140214.tar.bz2 http://downloads.sourceforge.net/pr
 tar xvjf scantailor-enhanced-20140214.tar.bz2
 cd scantailor-enhanced
 cmake .
-make -j
+make -j$2
 sudo make install
 }
