e20f80671c0ec1a9cfee0fe3ba644a490bcf4b1f
boredland <boredland@users.noreply.github.com>
2015-03-03 20:24:07 +0100

    Update fix_turbojpeg.sh

--- scripts/fix_turbojpeg.sh
+++ scripts/fix_turbojpeg.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 set -x
 platform='unknown'
 unamestr=`uname -i`
@@ -9,4 +8,8 @@ else if [[ "$unamestr" == 'armv7l' ]]; then
   sudo ln -s /usr/lib/arm-linux-gnueabihf/libturbojpeg.so.0.0.0 /usr/lib/arm-linux-gnueabihf/libturbojpeg.so
 else if [[ "$unamestr" == 'i386' ]]; then
 sudo ln -s /usr/lib/i386-linux-gnu/libturbojpeg.so.0.0.0 /usr/lib/i386-linux-gnu/libturbojpeg.so
+else then
+echo "Sadly there's no patch for your platform. Find libturbojpeg.so.0.0.0 and create sth. like 'sudo ln -s /usr/lib/i386-linux-gnu/libturbojpeg.so.0.0.0 /usr/lib/i386-linux-gnu/libturbojpeg.so
+ '"
+exit 0
 fi
