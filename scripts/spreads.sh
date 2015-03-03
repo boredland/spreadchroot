e813ae853d3cc4d53d9df2d587e7221d9e4feb70
boredland <boredland@users.noreply.github.com>
2015-03-04 00:42:38 +0100

    Update and rename scripts to scripts/spreads.sh

---
+++ scripts/spreads.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -x
+## Install spreads
+sudo pip install jpegtran-cffi Flask requests zipstream tornado Wand
+sudo pip install http://buildbot.diybookscanner.org/nightly/spreads-latest.tar.gz
