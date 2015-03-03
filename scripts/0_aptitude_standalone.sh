49d6749bb4016515ef8df093202d17d21823ac7a
boredland <boredland@users.noreply.github.com>
2015-03-03 19:01:09 +0100

    Update 0_aptitude_standalone.sh

--- scripts/0_aptitude_standalone.sh
+++ scripts/0_aptitude_standalone.sh
@@ -1,1 +1,12 @@
-0
+sudo apt-get install python2.7 python2.7-dev python-virtualenv \
+python-tk idle python-pmw python-imaging \
+python-pip libpython2.7-dev libusb-dev \
+libjpeg-dev libtiff5-dev libqt4-core ruby-rmagick \
+libmagickwand-dev lua5.2 liblua5.2-dev \
+ruby-hpricot git imagemagick build-essential \
+libqt4-dev libjpeg8-dev libjpeg-turbo8-dev libjpeg-dev git \
+ghostscript ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 \
+irb1.9.1 ri1.9.1 rdoc1.9.1 libopenssl-ruby1.9.1 libssl-dev \
+zlib1g-dev cmake zlib1g-dev libpng12-dev libtiff5-dev libhidapi-dev \
+libboost1.55-all-dev libxrender-dev libturbojpeg python-pyside \
+automake libtool libpango1.0-dev python-psutil libffi-dev -y
