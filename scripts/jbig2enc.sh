7d1e247e337e15c1b4d212fc0a62677a9e4b4f8c
boredland <boredland@users.noreply.github.com>
2015-03-03 19:45:07 +0100

    Update jbig2enc.sh

---
+++ scripts/jbig2enc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+set -x
+command -v jbig2 >/dev/null 2>&1 || {
+git clone https://github.com/agl/jbig2enc
+cd jbig2enc
+./autogen.sh
+./configure
+make -j
+sudo make install
+cd ..
+}
