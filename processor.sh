7e7f26e94f317a5baa14a561155920d2968e290f
boredland <boredland@users.noreply.github.com>
2015-03-04 00:04:14 +0100

    Update processor.sh

--- processor.sh
+++ processor.sh
@@ -6,7 +6,7 @@ threads=`cat /proc/cpuinfo | grep processor | wc -l`
 threads=$((threads - 1))
 ## general depencies
 sudo apt-get install python2.7-dev python-pip build-essential \
-pkg-config libffi-dev libmagickwand-dev python-cffi libturbojpeg -y
+pkg-config libffi-dev libmagickwand-dev python-cffi libturbojpeg rubygems1.9.1 -y
 ## building depencies
 sudo apt-get install automake cmake libqt4-dev libboost1.55-all-dev -y
 
