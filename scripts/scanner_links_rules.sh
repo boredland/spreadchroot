89dd0d493e9c2fa01ac4b48a23772b95950e17f2
boredland <boredland@users.noreply.github.com>
2015-03-03 22:25:41 +0100

    Update scanner_links_rules.sh

--- scripts/scanner_links_rules.sh
+++ scripts/scanner_links_rules.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 set -x
 if [[ $1 == 2 ]]; then exit 0;fi
-##create and open a new file - not necessary I think
+##autoload chdkptp
 if grep -q /usr/local/lib/chdkptp/ "/etc/ld.so.conf.d/spreads.conf"
 then
 sudo sh -c "echo '/usr/local/lib/chdkptp/' >> /etc/ld.so.conf.d/spreads.conf"
+fi
+## Add udev rule for hidtrigger
+if grep -q 'ACTION=="add", SUBSYSTEM=="usb", MODE:="666"' "/etc/udev/rules.d/99-usb.rules"
+then
+sudo sh -c "echo 'ACTION=="add", SUBSYSTEM=="usb", MODE:="666"' > /etc/udev/rules.d/99-usb.rules"
+sed -i -e 's/KERNEL\!="eth\*|/KERNEL\!="/' /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+fi
+## Add chdkptp to bash
+if grep -q "CHDKPTP_DIR=/usr/local/lib/chdkptp" ~/.bashrc
+then
+echo "export CHDKPTP_DIR=/usr/local/lib/chdkptp" >> ~/.bashrc 
 fi
