bd55789aec2d65c78f674664e3da01c22efa60af
boredland <boredland@users.noreply.github.com>
2015-03-04 00:34:47 +0100

    Update processor.sh

--- processor.sh
+++ processor.sh
@@ -7,7 +7,8 @@ threads=$((threads - 1))
 sudo apt-get install python2.7-dev python-pip build-essential \
 pkg-config libffi-dev libmagickwand-dev python-cffi libturbojpeg  -y
 ## building depencies
-sudo apt-get install automake cmake libqt4-dev libboost1.55-all-dev rubygems1.9.1 ruby1.9.1-dev imagemagick -y
+sudo apt-get install automake cmake libqt4-dev libboost1.55-all-dev \
+rubygems1.9.1 ruby1.9.1-dev imagemagick -y
 
 ## Compile and install latest leptonica
 ./scripts/leptonica.sh $threads
@@ -27,6 +28,13 @@ sudo apt-get install automake cmake libqt4-dev libboost1.55-all-dev rubygems1.9.
 ## Install Scantailor
 ./scripts/scantailor.sh $threads
 sudo ldconfig
+
+## Install spreads
+sudo pip install jpegtran-cffi Flask requests zipstream tornado Wand
+sudo pip install http://buildbot.diybookscanner.org/nightly/spreads-latest.tar.gz
+
 mkdir ~/.config
 mkdir ~/.config/spreads
+cp files/processor.yaml ~/.config/spreads/config.yaml
+
 exit 0
