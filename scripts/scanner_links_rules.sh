52c4078367e55018d1c2d70f8eb32e7b4520148f
boredland <boredland@users.noreply.github.com>
2015-03-03 23:01:36 +0100

    Update scanner_links_rules.sh

--- scripts/scanner_links_rules.sh
+++ scripts/scanner_links_rules.sh
@@ -4,11 +4,15 @@ if [[ $1 == 2 ]]; then exit 0;fi
 ##autoload chdkptp
 if grep -q /usr/local/lib/chdkptp/ "/etc/ld.so.conf.d/spreads.conf"
 then
+echo "## Already inserted /etc/ld.so.conf.d/spreads.conf"
+else
 sudo sh -c "echo '/usr/local/lib/chdkptp/' >> /etc/ld.so.conf.d/spreads.conf"
 fi
 ## Add udev rule for hidtrigger
 if grep -q 'ACTION=="add", SUBSYSTEM=="usb", MODE:="666"' "/etc/udev/rules.d/99-usb.rules"
 then
+echo "## Udev-Rule already applied."
+else
 sudo sh -c "echo 'ACTION=="add", SUBSYSTEM=="usb", MODE:="666"' > /etc/udev/rules.d/99-usb.rules"
 sed -i -e 's/KERNEL\!="eth\*|/KERNEL\!="/' /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
@@ -16,5 +20,7 @@ fi
 ## Add chdkptp to bash
 if grep -q "CHDKPTP_DIR=/usr/local/lib/chdkptp" ~/.bashrc
 then
+echo "chdkptp already in bashrc."
+else
 echo "export CHDKPTP_DIR=/usr/local/lib/chdkptp" >> ~/.bashrc 
 fi
