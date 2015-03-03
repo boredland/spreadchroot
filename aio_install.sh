da37c155f077a9f392e16474ea776b3d153857ed
boredland <boredland@users.noreply.github.com>
2015-03-03 19:14:12 +0100

    Update aio_install.sh

--- aio_install.sh
+++ aio_install.sh
@@ -4,36 +4,20 @@ set -x
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 exec > >(tee $(date +%F)_$(date +"%I-%M-%S")_spreads_deploy_log.txt)
 exec 2>&1
-##starting from a clean 14.04 system
-sudo apt-get update
-sudo apt-get upgrade	
 SCRIPTPATH="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )" && pwd )"
-##now install all dependcies
-sudo apt-get install python2.7 python2.7-dev python-virtualenv \
-python-tk idle python-pmw python-imaging \
-python-pip libpython2.7-dev libusb-dev \
-libjpeg-dev libtiff5-dev libqt4-core ruby-rmagick \
-libmagickwand-dev lua5.2 liblua5.2-dev \
-ruby-hpricot git imagemagick build-essential \
-libqt4-dev libjpeg8-dev libjpeg-turbo8-dev libjpeg-dev git \
-ghostscript ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 \
-irb1.9.1 ri1.9.1 rdoc1.9.1 libopenssl-ruby1.9.1 libssl-dev \
-zlib1g-dev cmake zlib1g-dev libpng12-dev libtiff5-dev libhidapi-dev \
-libboost1.55-all-dev libxrender-dev libturbojpeg python-pyside \
-automake libtool libpango1.0-dev python-psutil libffi-dev -y
-##python-usb has some depency-error
+
+echo "Do you want to install for capturing only[1], server only [2] or full [3]:"
+read mode
+
+echo "Please set the number of cores for the c-compiler:"
+read j
+
+## aptitude
+sh scripts/aptitude.sh $mode
 
 ##install latest leptonica
-command -v convertfilestopdf >/dev/null 2>&1 || {
-wget http://www.leptonica.com/source/leptonica-1.71.tar.gz
-tar xvf leptonica-1.71.tar.gz
-cd leptonica-1.71/
-./autobuild
-./configure
-make -j
-sudo make install
-cd ..
-}
+sh scripts/leptonica.sh $mode $j
+exit 0
 ## install tesseract from git
 command -v tesseract >/dev/null 2>&1 || {
 if [[ ! -d tesseract-ocr ]]
