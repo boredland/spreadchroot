52009d8a37e97031334e2bf5d70cac23e6d51b25
boredland <boredland@users.noreply.github.com>
2015-03-04 13:02:34 +0100

    Update scanner_links_rules.sh

--- scripts/scanner_links_rules.sh
+++ scripts/scanner_links_rules.sh
@@ -13,7 +13,7 @@ then
 echo "## Udev-Rule already applied."
 else
 sudo sh -c "echo 'ACTION=="add", SUBSYSTEM=="usb", MODE:="666"' > /etc/udev/rules.d/99-usb.rules"
-sed -i -e 's/KERNEL\!="eth\*|/KERNEL\!="/' /lib/udev/rules.d/75-persistent-net-generator.rules
+sudo sed -i -e 's/KERNEL\!="eth\*|/KERNEL\!="/' /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 sudo udevadm control --reload-rules
 fi
