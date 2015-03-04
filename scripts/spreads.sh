720d7e53625608539f582be8af9cb09714af582e
boredland <boredland@users.noreply.github.com>
2015-03-04 15:07:43 +0100

    Update spreads.sh

--- scripts/spreads.sh
+++ scripts/spreads.sh
@@ -2,5 +2,4 @@
 set -x
 ## Install spreads
 sudo pip install jpegtran-cffi Flask requests zipstream tornado Wand
-wget http://buildbot.diybookscanner.org/nightly/spreads-latest.tar.gz
-sudo pip install spreads-latest.tar.gz
+sudo pip install http://buildbot.diybookscanner.org/nightly/spreads-latest.tar.gz#egg=spreads[web]
