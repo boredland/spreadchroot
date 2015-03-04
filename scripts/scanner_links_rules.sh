08cdf1a1f92196bb7075289ecc6fe340d70e62dd
boredland <boredland@users.noreply.github.com>
2015-03-04 12:37:11 +0100

    Update scanner_links_rules.sh

--- scripts/scanner_links_rules.sh
+++ scripts/scanner_links_rules.sh
@@ -15,6 +15,7 @@ else
 sudo sh -c "echo 'ACTION=="add", SUBSYSTEM=="usb", MODE:="666"' > /etc/udev/rules.d/99-usb.rules"
 sed -i -e 's/KERNEL\!="eth\*|/KERNEL\!="/' /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
+sudo udevadm control --reload-rules
 fi
 ## Add chdkptp to bash
 if grep -q "CHDKPTP_DIR=/usr/local/lib/chdkptp" ~/.bashrc
