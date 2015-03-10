789fc0db9f09ac812393c2a5cef2807eaa8b9fc7
boredland <boredland@users.noreply.github.com>
2015-03-11 00:57:46 +0100

    Update jbig2enc.sh

--- scripts/jbig2enc.sh
+++ scripts/jbig2enc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -e
 set -x
 if [[ $2 == 1 ]]; then exit 0;fi
 command -v jbig2 >/dev/null 2>&1 || {
-  echo blubb2
 git clone https://github.com/agl/jbig2enc
 cd jbig2enc
 ./autogen.sh
