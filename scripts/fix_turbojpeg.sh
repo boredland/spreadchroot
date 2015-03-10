57557331d825e0b93109d8083370feb05d176ab1
boredland <boredland@users.noreply.github.com>
2015-03-10 18:19:27 +0100

    Update fix_turbojpeg.sh

--- scripts/fix_turbojpeg.sh
+++ scripts/fix_turbojpeg.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+set -e
 turbojpeg=`sudo find / -type f -name "libturbojpeg.so.0.0.0"`
 if [[ ! -f /usr/lib/x86_64-linux-gnu/libturbojpeg.so ]]; then
 sudo ln -s $turbojpeg ${turbojpeg%.0.0.0}
