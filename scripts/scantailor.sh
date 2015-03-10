45f3983d4c3e6784771004b796febf621c580e76
boredland <boredland@users.noreply.github.com>
2015-03-10 18:25:18 +0100

    Update scantailor.sh

--- scripts/scantailor.sh
+++ scripts/scantailor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 set -x
-if [[ $1 == 1 ]]; then exit 0;fi
+if [[ $2 == 1 ]]; then exit 0;fi
 command -v scantailor-cli >/dev/null 2>&1 || {
 wget -O scantailor-enhanced-20140214.tar.bz2 http://downloads.sourceforge.net/project/scantailor/scantailor-devel/enhanced/scantailor-enhanced-20140214.tar.bz2
 tar xvjf scantailor-enhanced-20140214.tar.bz2
