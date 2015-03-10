11a2341259019a97e27f343a9f5f42553a1c965e
boredland <boredland@users.noreply.github.com>
2015-03-10 18:24:30 +0100

    Update djvubind.sh

--- scripts/djvubind.sh
+++ scripts/djvubind.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 set -x
-if [[ $1 == 1 ]]; then exit 0;fi
+if [[ $2 == 1 ]]; then exit 0;fi
 command -v djvubind >/dev/null 2>&1 || {
 git clone https://github.com/strider1551/djvubind
 cd djvubind
