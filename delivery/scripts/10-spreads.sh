7e3a7450dc21f920fb8a230f92d736b96f53d025
boredland <boredland@users.noreply.github.com>
2015-06-22 11:01:26 +0000

    Update 10-spreads.sh

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -7,12 +7,14 @@ set -x
         python python-colorama python-yaml python-concurrent.futures \
         python-blinker python-roman python-usb python-psutil libjpeg-dev \
         python-flask python-requests python-wand python-netifaces \
-        python-dbus liblua5.2-dev libusb-dev python-cffi libturbojpeg-dev
+        python-dbus liblua5.2-dev libusb-dev python-cffi libturbojpeg-dev \
+        libgphoto2-dev 
     apt-get -y install python-pip build-essential python2.7-dev pkg-config
     pip install tornado
     pip install jpegtran-cffi
     pip install lupa --install-option="--no-luajit"
     pip install chdkptp.py
+    pip install gphoto2-cffi
 
 # Create spreads configuration directoy
 mkdir -p /home/spreads/.config/spreads
