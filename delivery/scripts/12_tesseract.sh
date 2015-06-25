8e933657bdced48e0facdbc272cea2ed14470721
Ubuntu <ubuntu@ip-172-31-6-132.eu-central-1.compute.internal>
2015-06-25 09:50:36 +0000

    back to tesseract 3.03. for compiling-speed. perhaps create 3.04. deb later.

--- delivery/scripts/12_tesseract.sh
+++ delivery/scripts/12_tesseract.sh
@@ -2,15 +2,16 @@
 set -x
 set -e
 
-git clone https://code.google.com/p/tesseract-ocr/
-cd tesseract-ocr
-git pull
-./autogen.sh
-./configure
-make -j$CORES
-make install
-cd ..
-rm -r tesseract-ocr
-git clone https://code.google.com/p/tesseract-ocr.tessdata/ tessdata
-cp tessdata/* /usr/local/share/tessdata
-rm -r tessdata
+#git clone https://code.google.com/p/tesseract-ocr/
+#cd tesseract-ocr
+#git pull
+#./autogen.sh
+#./configure
+#make -j$CORES
+#make install
+#cd ..
+#rm -r tesseract-ocr
+#git clone https://code.google.com/p/tesseract-ocr.tessdata/ tessdata
+#cp tessdata/* /usr/local/share/tessdata
+#rm -r tessdata
+apt-get -y --force-yes install tesseract-ocr tesseract-ocr-*
