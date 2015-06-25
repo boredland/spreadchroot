379676bc91f61ec9e9b464884c537d0397c3b907
Ubuntu <ubuntu@ip-172-31-6-132.eu-central-1.compute.internal>
2015-06-25 22:11:26 +0000

    added compile again. deb not working...

--- delivery/scripts/13_jbig2enc.sh
+++ delivery/scripts/13_jbig2enc.sh
@@ -2,12 +2,12 @@
 set -x
 set -e
 
-#git clone https://github.com/agl/jbig2enc
-#cd jbig2enc
-#./autogen.sh
-#./configure
-#make -j$CORES
-#make install
-#cd ..
-#rm -r jbig2enc
-dpkg -i $DELIVERY_DIR/files/jbig2enc_*_armhf.deb
+git clone https://github.com/agl/jbig2enc
+cd jbig2enc
+./autogen.sh
+./configure
+make -j$CORES
+make install
+cd ..
+rm -r jbig2enc
+#dpkg -i $DELIVERY_DIR/files/jbig2enc_*_armhf.deb
