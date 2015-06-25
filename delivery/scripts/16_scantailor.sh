082b3e0f782c883f335d365a7ed35b079dbe8ec4
Ubuntu <ubuntu@ip-172-31-6-132.eu-central-1.compute.internal>
2015-06-25 09:54:07 +0000

    removed scantailor enhanced source build, will add enhanced deb later

--- delivery/scripts/16_scantailor.sh
+++ delivery/scripts/16_scantailor.sh
@@ -4,12 +4,12 @@ set -e
 
 #wget -O scantailor-enhanced-20140214.tar.bz2 http://downloads.sourceforge.net/project/scantailor/scantailor-devel/enhanced/scantailor-enhanced-20140214.tar.bz2
 #tar xvjf scantailor-enhanced-20140214.tar.bz2
-git clone -b enhanced --single-branch https://github.com/scantailor/scantailor
-cd scantailor*
-cmake .
-make -j$CORES
-make install
-cd ..
-rm -r scantailor*
-
+#git clone -b enhanced --single-branch https://github.com/scantailor/scantailor
+#cd scantailor*
+#cmake .
+#make -j$CORES
+#make install
+#cd ..
+#rm -r scantailor*
+apt-get -y --force-yes install scantailor 
 
