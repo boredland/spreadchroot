1e7a7c16cf279565187fda0f1fb316cc4ea858d3
Jonas Strassel <boredland@users.noreply.github.com>
2015-06-26 16:41:32 +0200

    Update 15_djvubind.sh

--- delivery/scripts/15_djvubind.sh
+++ delivery/scripts/15_djvubind.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 set -x
-apt-get -y --force-yes install djvulibre-bin minidjvu python3-minimal
-git clone https://github.com/strider1551/djvubind
-cd djvubind
-./setup.py install
-cd ..
-rm -r djvubind
+apt-get -y --force-yes install djvubind minidjvu
+#git clone https://github.com/strider1551/djvubind
+#cd djvubind
+#./setup.py install
+#cd ..
+#rm -r djvubind
