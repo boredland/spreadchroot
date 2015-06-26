188480d0f1236fd5cfb46288fe77d1591f4042ca
Jonas Strassel <boredland@users.noreply.github.com>
2015-06-26 02:33:10 +0200

    Update 03-tools.sh

--- delivery/scripts/03-tools.sh
+++ delivery/scripts/03-tools.sh
@@ -7,7 +7,7 @@ apt-get -y install --no-install-recommends \
     cifs-utils htop less ntp openssh-server sudo nano \
     bash-completion automake cmake libboost1.55-all-dev \
     ruby-dev rubygems ruby imagemagick libtool libleptonica-dev git-core zlib1g-dev \
-    selinux-basics selinux-utils checkpolicy libapol4 libqpol1 policycoreutils psmisc
+    selinux-basics selinux-utils checkpolicy libapol4 libqpol1 policycoreutils psmisc #linux deploy depencies
     
 # 
 # User spreads should be able to shut the system down (used in web interface)
