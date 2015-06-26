c3316efb93bc3f9519843e00e457c121e41d3468
Jonas Strassel <boredland@users.noreply.github.com>
2015-06-26 17:01:49 +0200

    Update 99_cleanup.sh

--- delivery/scripts/99_cleanup.sh
+++ delivery/scripts/99_cleanup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
 set -e
-apt-get -y remove --purge --auto-remove build-essential cmake automake git* git-core
+apt-get -y remove --purge --auto-remove build-essential cmake automake git*
 apt-get -y autoremove
 apt-get clean
