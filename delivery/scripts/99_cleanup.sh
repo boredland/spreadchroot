34275b2001315b93c5eaffb0aa0b90547647e61e
Jonas Strassel <boredland@users.noreply.github.com>
2015-06-26 03:03:36 +0200

    Update 99_cleanup.sh

--- delivery/scripts/99_cleanup.sh
+++ delivery/scripts/99_cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 set -e
-apt-get -y remove --purge --auto-remove build-essential cmake automake git-core libtool \
+apt-get -y remove --purge --auto-remove build-essential cmake automake git* libtool \
 libleptonica-dev git-core zlib1g-dev ruby-dev rubygems
 apt-get -y autoremove
 apt-get clean
