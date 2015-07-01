a8bc80c38a767a9bcbad306feb52aeb1178b262a
Jonas Strassel <boredland@users.noreply.github.com>
2015-07-01 09:04:53 +0200

    Update 99_cleanup.sh

--- delivery/scripts/99_cleanup.sh
+++ delivery/scripts/99_cleanup.sh
@@ -4,7 +4,7 @@ set -e
 apt-get -y remove --purge --auto-remove build-essential automake \
   git-core zlib1g-dev ruby-dev rubygems libtool build-essential \
   automake cmake libffi-dev libturbojpeg-dev libmagickwand-dev \
-  libmagickwand-dev libxslt-dev libxml2-dev libgphoto2-dev \
+  libmagickwand-dev libxslt-dev libxml2-dev \
   tesseract-ocr-dev
 apt-get -y --force-yes autoremove
 apt-get clean
